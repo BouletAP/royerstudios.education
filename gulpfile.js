391eaaa3bd7aa4f2d40ab167ddd61ebf24d25cee
BouletAP <apb@bouletap.com>
2021-03-15 15:12:33 -0400

    Préparation du dev

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./_scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('bs', function() {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("./_scss/*.scss", gulp.series('sass'));
+
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("./*.css").on('change', browserSync.reload);
+});
+
+
+gulp.task('watcher', gulp.parallel ('sass', 'bs')); 
